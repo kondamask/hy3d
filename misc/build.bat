528ca2a090f3576d2247b3329d69281b6c1884db
heyyod <34712364+heyyod@users.noreply.github.com>
2020-10-17 00:08:39 +0300

    vs code and build changes. game now runs straight into vs code

--- misc/build.bat
+++ misc/build.bat
@@ -4,7 +4,8 @@ if not exist ..\data mkdir ..\data
 if not exist ..\..\build mkdir ..\..\build
 
 pushd ..\..\build
-cl  -MTd -WL -Od -nologo -fp:fast -fp:except- -Gm- -EHsc -Zo -Oi -WX -W4 -wd4100 -wd4458 -FC -Zi -GS-^
+cl  -MTd -WL -Od -nologo -fp:fast -fp:except- -Gm- -EHsc -Zo -Oi^
+    -W4 -wd4100 -wd4458 -FC -Zi -GS-^
     ..\hy3d\code\hy3d.cpp^
-    -link -incremental:no -opt:ref user32.lib gdi32.lib
+    -link -subsystem:windows -incremental:no -opt:ref user32.lib gdi32.lib kernel32.lib
 popd
